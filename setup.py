5b2a66ae098ba2b2bfc6a747a73a17bed6402594
i30817 <i30817@gmail.com>
2018-05-12 18:57:54 +0100

    forgot dependency

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['beautifulsoup4', 'lxml'],
+    install_requires=['beautifulsoup4', 'lxml', 'pyparsing'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
