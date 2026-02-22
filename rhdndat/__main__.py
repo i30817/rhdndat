669f7b5007346bbb9e45bc77133e9fdfb93ff5b2
i30817 <i30817@gmail.com>
2026-02-22 09:47:25 +0000

    Fix list concatenation

--- rhdndat/__main__.py
+++ rhdndat/__main__.py
@@ -355,7 +355,7 @@ def renamer(romdir: Path = typer.Argument(..., exists=True, file_okay=False, dir
     if xmlpath.is_file():
         xmls = [xmlpath]
     elif xmlpath.is_dir():
-        xmls = list(xmlpath.glob('**/*.dat') + xmlpath.glob('**/*.xml'))
+        xmls = list(xmlpath.glob('**/*.dat')) + list(xmlpath.glob('**/*.xml'))
     if not xmls:
         error('Can\'t find xml dats in second argument')
         raise typer.Abort()
